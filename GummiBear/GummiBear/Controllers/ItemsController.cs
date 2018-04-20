68b66771abb5bdde70ab2c69fd05d8200a46b211
Frank Ngo <Frankngomusic@gmail.com>
2018-04-20 12:02:40 -0700

    add view for new list of items route

---
+++ GummiBear/GummiBear/Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GummiBear.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GummiBear.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private StoreDbContext db = new StoreDbContext();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Item> model = db.Items.ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+    }
+}
--- GummiBear/GummiBear/Models/Item.cs
+++ GummiBear/GummiBear/Models/Item.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
 
 namespace GummiBear.Models
 {
+    [Table("Items")]
     public class Item
     {
         [Key]
         public int ItemId { get; set; }
         public string Name { get; set; }
-        public string cost { get; set; }
-        public string description { get; set; }
+        public string Cost { get; set; }
+        public string Description { get; set; }
     }
 }
