7ef0f4fc1dd0429540941b4636a8495442aa99a0
Frank Ngo <Frankngomusic@gmail.com>
2018-04-27 16:54:49 -0700

    add test and all test pass for Reviews model

---
+++ GummiBear/GummiBear.Test/ModelTests/ReviewTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GummiBear.Models;
+
+namespace GummiBear.Tests
+{
+    [TestClass]
+    public class ReviewTests
+    {
+        [TestMethod]
+        public void GetAuthor_ReturnsReviewAuthor_String()
+        {
+            //Arrange
+            var review = new Review();
+
+            //Act
+            review.Author = "Jake";
+            var result = review.Author;
+
+            //Assert
+            Assert.AreEqual("Jake", result);
+        }
+
+        [TestMethod]
+        public void GetContent_ReturnsReviewContent_String()
+        {
+            //Arrange
+            var review = new Review();
+
+            //Act
+            review.Content = "It was ok";
+            var result = review.Content;
+
+            //Assert
+            Assert.AreEqual("It was ok", result);
+        }
+
+        [TestMethod]
+        public void GetRating_ReturnsReviewRating_String()
+        {
+            //Arrange
+            var review = new Review();
+
+            //Act
+            review.Rating = "5";
+            var result = review.Rating;
+
+            //Assert
+            Assert.AreEqual("5", result);
+        }
+
+    }
+}
