d8144957838542a6f9496d04cd4a9bdcf259a173
Frank Ngo <Frankngomusic@gmail.com>
2018-04-27 15:12:37 -0700

    add test and pass test for all Item variables

--- GummiBear/GummiBear.Test/ModelTests/ItemTests.cs
+++ GummiBear/GummiBear.Test/ModelTests/ItemTests.cs
@@ -15,10 +15,38 @@ namespace GummiBear.Tests
             //Act
             item.Name = "Blender";
             var result = item.Name;
-         
-
+        
             //Assert
             Assert.AreEqual("Blender", result);
         }
+
+        [TestMethod]
+        public void GetCost_ReturnsItemCost_String()
+        {
+            //Arrange
+            var item = new Item();
+
+            //Act
+            item.Cost = "10.00";
+            var result = item.Cost;
+
+            //Assert
+            Assert.AreEqual("10.00", result);
+        }
+
+        [TestMethod]
+        public void GetDescription_ReturnsItemDescription_String()
+        {
+            //Arrange
+            var item = new Item();
+
+            //Act
+            item.Description = "A portable blender";
+            var result = item.Description;
+
+            //Assert
+            Assert.AreEqual("A portable blender", result);
+        }
+
     }
 }
