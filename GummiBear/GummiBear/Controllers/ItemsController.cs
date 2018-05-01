1f5c2395c878fb30225192344274c7ca6585c5c8
Frank Ngo <frankngomusic@gmail.com>
2018-05-01 15:26:07 -0700

    update methods for interface repos

--- GummiBear/GummiBear/Controllers/ItemsController.cs
+++ GummiBear/GummiBear/Controllers/ItemsController.cs
@@ -13,67 +13,103 @@ namespace GummiBear.Controllers
     public class ItemsController : Controller
     {
         private StoreDbContext db = new StoreDbContext();
+
+        private IItemRepository itemRepo;
+
+        public ItemsController(IItemRepository repo = null)
+        {
+            if (repo == null)
+            {
+                this.itemRepo = new EFItemRepository();
+            }
+            else
+            {
+                this.itemRepo = repo;
+            }
+        }
+
+
         public IActionResult Index()
         {
        
-            return View(db.Items.Include(items => items.Reviews).ToList());
+            //return View(db.Items.Include(items => items.Reviews).ToList());
+
+            return View(itemRepo.Items.ToList());
         }
 
         public IActionResult Details(int id)
         {
            
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            //var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            //return View(thisItem);
+
+            Item thisItem = itemRepo.Items.FirstOrDefault(x => x.ItemId == id);
             return View(thisItem);
         }
 
         public IActionResult Create()
         {
-            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Name");
             return View();
         }
 
         [HttpPost]
         public IActionResult Create(Item item)
         {
-            db.Items.Add(item);
-            db.SaveChanges();
+            //db.Items.Add(item);
+            //db.SaveChanges();
+            //return RedirectToAction("Index");
+
+            itemRepo.Save(item);   // Updated
+            // Removed db.SaveChanges() call
             return RedirectToAction("Index");
         }
 
         public IActionResult Edit(int id)
         {
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Name");
+            //var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            //ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Name");
+            //return View(thisItem);
+
+            Item thisItem = itemRepo.Items.FirstOrDefault(x => x.ItemId == id);
             return View(thisItem);
         }
 
         [HttpPost]
         public IActionResult Edit(Item item)
         {
-            db.Entry(item).State = EntityState.Modified;
-            db.SaveChanges();
+            //db.Entry(item).State = EntityState.Modified;
+            //db.SaveChanges();
+            //return RedirectToAction("Index");
+
+            itemRepo.Edit(item);   // Updated!
+            // Removed db.SaveChanges() call
             return RedirectToAction("Index");
         }
 
         public IActionResult Delete(int id)
         {
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            //var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            //return View(thisItem);
+
+            Item thisItem = itemRepo.Items.FirstOrDefault(x => x.ItemId == id);
             return View(thisItem);
         }
 
         [HttpPost, ActionName("Delete")]
         public IActionResult DeleteConfirmed(int id)
         {
-            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-            db.Items.Remove(thisItem);
-            db.SaveChanges();
+            //var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            //db.Items.Remove(thisItem);
+            //db.SaveChanges();
+            //return RedirectToAction("Index");
+
+            Item thisItem = itemRepo.Items.FirstOrDefault(x => x.ItemId == id);
+            itemRepo.Remove(thisItem);   // Updated!
+            // Removed db.SaveChanges() call
             return RedirectToAction("Index");
         }
 
 
-        public IActionResult Item()
-        {
-            return View("../Items/Index");
-        }
+
     }
 }
